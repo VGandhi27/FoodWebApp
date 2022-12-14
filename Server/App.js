d81442c9bbeb4718b5e887290f6da47006c1de78
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-12-14 23:05:25 +0530

    working on deployment4

--- Server/App.js
+++ Server/App.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app=express()
 const mongoose =require('mongoose')
 const PORT =4000
-const {MONGOURI}=require("./config/key")
+const {MONGOURI}=require("./Key")
 
 
 
---
+++ Server/Key.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://VidushiGandhi:YhbuK3ov4gZHlJxG@cluster0.x9v8k85.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"gdjhvvdhjddhdbhdbjhonsbdvuhbqwnkqxsab"
+}
